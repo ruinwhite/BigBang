6e769c81aa9126b392d078a896a5845842077006
孔瑞 <@DDT-PC>
2016-08-23 01:12:20 +0800

    新增随便目录 完成登录action 完成登录映射配置

--- src/main/java/cn/com/ruin/action/LoginAction.java
+++ src/main/java/cn/com/ruin/action/LoginAction.java
@@ -1,6 +1,7 @@
 package cn.com.ruin.action;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,11 @@ public class LoginAction extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+		resp.setContentType("text/html;charset=UTF-8");
+		String name = req.getParameter("name");
+		String pwd = req.getParameter("password");
+		//TODO service接口完成
+		resp.sendRedirect("/husky/essay/public/html/public.html");
 	}
 
 }
